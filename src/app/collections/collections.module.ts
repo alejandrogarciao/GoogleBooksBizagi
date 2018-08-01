a04e38074f8bccde9948b04cf7a16157e1369b44
johnrsierra <john.r.sierra@gmail.com>
2018-08-01 14:00:02 -0500

    Fix module declaration for collections and favorites

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CollectionListAppComponent } from './containers/collection-list-app/collection-list-app.component'
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [CollectionListAppComponent]
 })
 export class CollectionsModule { }
--- src/app/favorites/favorites.module.ts
+++ src/app/favorites/favorites.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FavoritesListAppComponent } from './containers/favorites-list-app/favorites-list-app.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [FavoritesListAppComponent]
 })
 export class FavoritesModule { }
