1b297472f601a6e12b43dfc47d4fb57a6ba6ab8b
johnrsierra <john.r.sierra@gmail.com>
2018-08-08 14:14:46 -0500

    Favorites components added

--- src/app/favorites/favorites.module.ts
+++ src/app/favorites/favorites.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FavoritesListAppComponent } from './containers/favorites-list-app/favorites-list-app.component';
+import { RouterModule } from "@angular/router";
+
+import { routes } from "./routes.favorites";
+import { FavoritesListAppComponent } from "./containers/favorites-list-app/favorites-list-app.component";
+import { CardBookComponent } from './components/card-book/card-book.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: [FavoritesListAppComponent]
+  declarations: [FavoritesListAppComponent, CardBookComponent]
 })
 export class FavoritesModule { }
