6b503edf8a133e6ac664ac9e72339864877fcd5f
johnrsierra <john.r.sierra@gmail.com>
2018-08-09 14:48:50 -0500

    Favorites and detail css fixes

--- src/app/favorites/components/card-book/card-book.component.ts
+++ src/app/favorites/components/card-book/card-book.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-card-book',
   templateUrl: './card-book.component.html',
-  styleUrls: ['./card-book.component.styl']
+  styleUrls: ['./card-book.component.css']
 })
 export class CardBookComponent implements OnInit {
 
