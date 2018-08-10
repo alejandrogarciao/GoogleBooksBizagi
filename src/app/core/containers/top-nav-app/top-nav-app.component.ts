4eda2225fc7eab76b3b1a78eb18a52b7f3975de9
Oscar Alejandro Garcia Oviedo <Oscar.Garcia@bizagi.com>
2018-08-09 19:21:11 -0500

    User Image top nav

--- src/app/core/containers/top-nav-app/top-nav-app.component.ts
+++ src/app/core/containers/top-nav-app/top-nav-app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { BooksListService } from '../../../books/services/list/books-list.service';
+import { AuthService } from "../../../auth/services/auth/auth.service";
+import { User } from "firebase/app";
+import { AngularFireAuth } from "angularfire2/auth";
 
 @Component({
   selector: 'top-nav-app',
@@ -7,15 +10,29 @@ import { BooksListService } from '../../../books/services/list/books-list.servic
   styleUrls: ['./top-nav-app.component.css']
 })
 export class TopNavAppComponent implements OnInit {
+  user: User;
+  today: Date;
+ 
+  constructor(private authFire:  AngularFireAuth,private authService: AuthService,private _bookService: BooksListService) {
+    this.today = new Date();
+    this.state = 'open';
+   }
+
+  logout(){
+    this.authService.logout();
+  }
 
   @Output() actionAside = new EventEmitter<string>();
   state: string;
 
-  constructor(private _bookService: BooksListService) { 
-    this.state = 'open';
-  }
 
   ngOnInit() {
+    this.authFire.authState
+    .subscribe(
+      user => {        
+        this.user = user;
+      }
+    )
   }
 
   closeAside() {
