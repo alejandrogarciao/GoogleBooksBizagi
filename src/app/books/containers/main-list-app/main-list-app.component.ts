d1e4023264b2292814cf7d1d82d831a967d6aeb2
Oscar Alejandro Garcia Oviedo <Oscar.Garcia@bizagi.com>
2018-08-15 17:38:00 -0500

    Styles collection section

--- src/app/books/containers/main-list-app/main-list-app.component.ts
+++ src/app/books/containers/main-list-app/main-list-app.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 //import { books } from "../../../data-books";
 import { BooksListService } from "../../services/list/books-list.service";
+import { AngularFireAuth } from "angularfire2/auth";
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
 import { BookList } from '../../models';
+import { Observable } from 'rxjs';
 
 @Component({
   //selector: 'app-books-main',
@@ -11,14 +14,31 @@ import { BookList } from '../../models';
 })
 export class MainListAppComponent implements OnInit {
   display = "none";
-  value:string;
-  book:any;
+  value: string;
+  book: any;
   booksList: BookList;
-  @Input() collection:any;
+  categoriesList: Observable<any[]>;
+  selectedValue: string;
+  @Input() collection: any;
   @Output() pushCollection = new EventEmitter<any>();
+  user: firebase.User;
 
-  constructor(private bookService: BooksListService) {
+  constructor(private bookService: BooksListService, private authFire: AngularFireAuth,
+    private rdb: AngularFireDatabase) {
     this.bookService.searchBooks('Colombia');
+    this.categoriesList = null;
+    this.selectedValue = null;
+    this.authFire.authState
+    .subscribe(
+      user => {
+        this.user = user;
+        this.categoriesList = this.rdb.list('/collections/' + this.user.uid + "/").valueChanges();
+        let listCateg = this.rdb.list('/collections/' + this.user.uid + "/");
+        debugger;
+        console.log("collsRef: " + this.categoriesList);
+        console.log("collsRef: " + listCateg);
+      }
+    );
   }
   ngOnInit() {
     this.bookService.booksList
@@ -31,29 +51,29 @@ export class MainListAppComponent implements OnInit {
       );
   }
 
-  addFavorite(book: any){
+  addFavorite(book: any) {
     this.bookService.addFavorites(book);
   }
 
   openModal(book: any) {
-    this.display="block"; 
+    this.display = "block";
     this.book = book;
   }
 
   onCloseHandled() {
-    this.display="none"; 
+    this.display = "none";
   }
 
-  addBookToCategory(nameCategory:string){
+  addBookToCategory(nameCategory: string) {
     console.log(this.value);
-    this.bookService.addCollections(nameCategory,this.book);
+    this.bookService.addCollections(nameCategory, this.book);
     //this.pushCollection.emit(nameCategory+"/"+this.book);
-    this.display="none";
+    this.display = "none";
     /*
     console.log("idBook: " +  this.idBookCat + " - " + "idCategory: " + idCategory);
      
     */
   }
 
- 
+
 }
---
+++ src/app/books/services/categories/categories.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable, of, Subject } from 'rxjs';
+import { AngularFireAuth } from "angularfire2/auth";
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
+import * as firebase from "firebase";
+import { MessagesService } from "../../../alerts/services/messages.service";
+import { environment } from "../../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+  categories: Subject<any[]> = new Subject();
+  url = environment.apiBooks;
+  catRef: AngularFireList<any>;
+  user: firebase.User;
+  constructor(private http: HttpClient, private alertService: MessagesService, private authFire: AngularFireAuth,
+    rdb: AngularFireDatabase) {
+    this.categories.next();
+    authFire.authState
+      .subscribe(
+        user => {
+          this.user = user;
+          this.catRef = rdb.list('collections/' + this.user.uid);
+          debugger;
+          console.log("collections: " + this.catRef);
+        }
+      );
+     }
+}
